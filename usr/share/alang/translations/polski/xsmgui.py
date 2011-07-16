724dd276c8369cde3a6578586e4e9654abfe6156
webnull <webnull.www@gmail.com>
2011-07-16 13:54:44 +0200

    First HTML site test

---
+++ usr/share/alang/translations/polski/xsmgui.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+class alangINC():
+        def return_array(a):
+                MyLang = range(11)
+                MyLang[1] = 'Wyłącz komputer'
+                MyLang[2] = 'Uruchom ponownie'
+                MyLang[3] = 'Wyloguj'
+                MyLang[4] = 'Zablokuj ekran'
+                MyLang[5] = 'Zakończ sesję'
+                MyLang[6] = '[xsm-gui.py] Aplikacja xscreensaver nie została odnaleziona wśrod procesów systemowych, funkcja blokowania ekranu zostanie wyłączona.'
+                MyLang[7] = 'Zahibernuj'
+                MyLang[8] = 'Uśpij'
+                MyLang[9] = 'Szybki restart'
+                MyLang[10] = 'Załaduj ponownie bieżący system operacyjny'
+                return MyLang
