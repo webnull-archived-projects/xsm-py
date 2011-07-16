401c18cecadeb51c72bc0e59ec7d3e72377a5805
webnull <webnull.www@gmail.com>
2011-07-16 13:29:04 +0200

    Version 2.0 moved to GIT, implements kexec, two gui styles

---
+++ install.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+if [ "`whoami`" != "root" ]
+then
+    echo "Installer works only from root account."
+    exit
+fi
+
+originalDir=`pwd`
+echo "Preparing X-Session Manager in Python to be installed..."
+echo "Checking dependencies..."
+
+# install alang-py (dependency required)
+if [ ! -f "/usr/share/alang/python/alang.py" ]
+then
+    echo "Alang-py not found, installing..."
+    mkdir /tmp/alang
+    cd /tmp/alang
+    git clone git://github.com/webnull/alang-py.git
+    ./install.sh
+    cd "$originalDir"
+    rm -rf /tmp/alang
+    if [ -f "/usr/share/alang/python/alang.py" ]
+    then
+        echo -n " [OK]"
+    else
+        echo -n " [FAILED]\nInstallation failed, are you root?"
+    fi
+fi
+
+# install optional dependency if user will agree
+if [ -e "/usr/lib/libpybootutils" ]
+then
+    echo "Libpybootutils already installed."
+else
+    echo "Libpybootutils package is required for kexec feature, do you want to install it? [y/n]: "
+    read option
+
+    if [ "$option" == "y" ]
+    then
+        echo "Downloading libpybootutils..."
+        mkdir /tmp/libpybootutils
+        cd /tmp/libpybootutils
+        git clone git://github.com/webnull/libpybootutils.git
+        cp /tmp/libpybootutils/usr/lib/pybootutils /usr/lib/pybootutils -r
+        rm -rf /tmp/libpybootutils
+        if [ -e "/usr/lib/libpybootutils" ]
+        then
+            echo -n " [OK]"
+        else
+            echo -n " [FAILED]"
+        fi
+    fi
+fi 
+
+echo "Dependencies check finished."
+echo "Copying XSM files..."
+
+chmod +x usr/bin/* # make all scripts executable
+echo "Copying binaries..."
+cp usr/bin/* /usr/bin/ # copy binary files
+echo "Copying icons, and locales..."
+cp usr/share/* /usr/share -r # copy language files, icons
+echo "Creating system groups..."
+groupadd xsmpy
+echo "Adding entry to /etc/sudoers..."
+echo "%xsmpy ALL(root) NOPASSWD: /usr/bin/xsm-exit" >> /etc/sudoers
+echo "Please assign users to xsmpy group to allow them using xsm."
+
+if [ -f /usr/bin/xsm-exit ]
+then
+    echo "X-Session Manager Python installed on your system."
+else
+    echo "Something went wrong, XSM wasnt installed correctly. Are you root?"
+fi
---
+++ usr/share/alang/translations/english/xsmgui.py
@@ -0,0 +1,14 @@
+class alangINC():
+	def return_array(a):
+		MyLang = range(11)
+		MyLang[1] = 'Shutdown'
+		MyLang[2] = 'Reboot'
+		MyLang[3] = 'Logout'
+		MyLang[4] = 'Lock screen'
+		MyLang[5] = 'Exit Session'
+		MyLang[6] = '[xsm-gui.py] No xscreensaver found to be running, disabling #btn4 with Locking screen option'
+		MyLang[7] = 'Hibernate'
+		MyLang[8] = 'Sleep'
+		MyLang[9] = 'Quick restart'
+		MyLang[10] = 'Reload currently running operating system'
+		return MyLang
