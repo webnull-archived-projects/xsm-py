f6f63003cdd455a9da15eabc49e00a3ae356b598
webnull <webnull.www@gmail.com>
2011-07-25 22:45:06 +0200

    v2.3 - More configurable interface

--- usr/share/alang/translations/english/xsmgui.py
+++ usr/share/alang/translations/english/xsmgui.py
@@ -1,3 +1,18 @@
+#############################################################################
+##                                                                         ##
+##  Copyleft by WebNuLL < webnull.www at gmail dot com >                   ##
+##                                                                         ##
+## This program is free software; you can redistribute it and/or modify it ##
+## under the terms of the GNU General Public License version 3 as          ##
+## published by the Free Software Foundation; version 3.                   ##
+##                                                                         ##
+## This program is distributed in the hope that it will be useful, but     ##
+## WITHOUT ANY WARRANTY; without even the implied warranty of              ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       ##
+## General Public License for more details.                                ##
+##                                                                         ##
+#############################################################################
+
 class alangINC():
 	def return_array(a):
 		MyLang = range(11)
